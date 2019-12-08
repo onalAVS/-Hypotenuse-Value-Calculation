30a96f831c30dae5c8536435ca8647c5a91e64ef
onalAVS <56044448+onalAVS@users.noreply.github.com>
2019-12-08 18:48:52 +0300

    Add files via upload

---
+++ body mass.py
@@ -0,0 +1,38 @@
+print("Welcome to Body-Mass Index Calculation")
+
+weight = input("Enter Weight = ")
+
+height = input("Enter your height (cm) = ")
+
+if not bool(weight) or not bool(height):
+    print("Incorrect entry !!!")
+    exit()
+
+weight = float(weight)
+height = float(height)
+
+height = height/100
+
+result = weight/(height**2)
+
+print("Weight : {}\nHeight : {} (m)\nYour Body-Mass Index = {}".format(weight,height,result))
+
+
+
+
+if result < 18.5:
+    print("Weak")
+
+elif result >= 18.5 and result < 25:
+    print("Normal")
+
+elif result >= 25 and result <= 30:
+    print("Overweight")
+
+elif result > 30:
+    print("Obese")
+
+
+
+
+
