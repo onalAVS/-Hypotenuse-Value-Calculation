4682dc7db2db0a0d18fb28de9b310bec2873bc0c
onalAVS <omaronaltrs@gmail.com>
2019-12-08 17:28:13 +0200

    github a

---
+++ hypotenuse.py
@@ -0,0 +1,8 @@
+edge_a = int(input("a Enter the dimension of the edge = "))
+
+edge_b = int(input("b Enter the dimension of the edge = "))
+
+hypotenuse_c = (edge_a**2+edge_b**2)**0.5
+
+print("Hypotenuse Value = {} ".format(hypotenuse_c))
+
