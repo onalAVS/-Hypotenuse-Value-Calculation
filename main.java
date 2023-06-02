c4f5a1b3326b6e4df8a1324d19d2bff4be509dfd
Filayeng <64534466+Filayeng@users.noreply.github.com>
2023-06-02 21:50:04 +0300

    Create main.java 1 pr

---
+++ main.java
@@ -0,0 +1,10 @@
+public class RangePrinter {
+    public static void main(String[] args) {
+        int start = 1;
+        int end = 9;
+        
+        for (int i = start; i <= end; i++) {
+            System.out.println(i);
+        }
+    }
+}
