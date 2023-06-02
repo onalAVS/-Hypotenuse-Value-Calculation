6aebe20ccfb075a633f63662828526a963c7e364
Filayeng <64534466+Filayeng@users.noreply.github.com>
2023-06-02 20:29:57 +0300

    Update hypotenuse.py 2 pr

--- hypotenuse.py
+++ hypotenuse.py
@@ -2,7 +2,7 @@ edge_A = int(input("a Enter the dimension of the edge = "))
 
 edge_b = int(input("b Enter the dimension of the edge = "))
 
-hypotenuse_c = (edge_A ** 2+edge_B **2 )**0.5  //calculator
+hypotenuse_c = (edge_A ** 2+edge_B **2 )**0.5  //calculator for square areas
 
 print("Hypotenuse Value = {} ".format(hypotenuse_c))
 
