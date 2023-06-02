940dae63f5e29fca0c33ee56a4536996fb222e55
Filayeng <64534466+Filayeng@users.noreply.github.com>
2023-06-02 20:14:53 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -11,3 +11,17 @@ circle_perimeter, circle_area = calculate_circle(radius)
 
 print("Perimeter of the circle:", circle_perimeter)
 print("Area of the circle:", circle_area)
+
+def calculate_square(side_length):
+    perimeter = 4 * side_length
+    area = side_length ** 2
+    return perimeter, area
+
+# Example usage
+side_length = float(input("Enter the side length of the square: "))
+square_perimeter, square_area = calculate_square(side_length)
+
+print("Perimeter of the square:", square_perimeter)
+print("Area of the square:", square_area)
+
+
